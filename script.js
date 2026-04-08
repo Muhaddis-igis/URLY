589ac4cfdcb0947f018aeb11813d1134f975f3e2
Muhaddis-igis <mmuhaddis.be25igis@student.nust.edu.pk>
2026-04-08 17:15:52 +0500

    fix:testing

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const port = process.env.PORT || 3000
 
 dotenv.config()
 app.use(express.static('public'))
+app.set('views', './Views');
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(session({
